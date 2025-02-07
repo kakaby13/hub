4dd4e7059265f7ef88b355bfa1c0d6e9b621afb8
Vlad Gavrilchik <v.gavrilchik@itransition.com>
2025-02-07 22:59:31 +0300

    4
    4

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from gpiozero import AngularServo
+from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
 
-# Initialize the servo on GPIO pin 14
-# min_pulse_width and max_pulse_width may need to be adjusted for your servo
-servo = AngularServo(14, min_angle=0, max_angle=180, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
+# Initialize the pigpio factory
+factory = PiGPIOFactory()
+
+# Initialize the servo on GPIO pin 14 with pigpio factory
+servo = AngularServo(14, min_angle=0, max_angle=180, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000, pin_factory=factory)
 
 # Function to set the servo angle
 def set_angle(angle):
